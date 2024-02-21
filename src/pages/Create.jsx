01c4be074a7f82554cf14c93ec0dbe87895af1b3
Arslan <arslanabbas5511@gmail.com>
2024-02-21 22:04:19 +0500

    set pages syntax and styles in app.css

---
+++ src/pages/Create.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div className='page create'>
+        <h2>create</h2>      
+    </div>
+  )
+}
+
+export default Create
---
+++ src/pages/Update.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Update() {
+  return (
+    <div className='page update'>
+        <h2>Update</h2>
+    </div>
+  )
+}
+
+export default Update
